30d5cceb44f2f765d9aa5d528153d250a9898ae6
shynome <shynome@gmail.com>
2024-03-03 13:39:30 +0800

    添加tags显示

--- src/routes/blog/[year]/[post]/+page.svelte
+++ src/routes/blog/[year]/[post]/+page.svelte
@@ -2,10 +2,16 @@
 	import 'highlight.js/styles/default.min.css'
 	export let data
 	const post = data.post
+	const tags = post.attributes.tags.filter((t) => !t.startsWith('#'))
 </script>
 
 <div class="container">
 	<h1>{post.attributes.title}</h1>
+	<div>
+		{#each tags as tag}
+			<a href="/blog/tags/{tag}/" class="btn btn-sm btn-outline-secondary me-2">{tag}</a>
+		{/each}
+	</div>
 	<hr />
 	<div>
 		{@html data.content}
